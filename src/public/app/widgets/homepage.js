bc7be3ebf9ecd1aaaa1349aa50524de0a04079a6
vaifix <vaifix@163.com>
2024-07-27 23:13:49 +0800

    增加打开主页的延迟，如果去掉会有问题，暂未找到原因

--- src/public/app/widgets/homepage.js
+++ src/public/app/widgets/homepage.js
@@ -1,4 +1,3 @@
-
 import RightPanelWidget from "./right_panel_widget.js";
 import options from "../services/options.js";
 import appContext from "../components/app_context.js";
@@ -16,19 +15,19 @@ export default class HomepageWidget extends RightPanelWidget {
         if (this.isEnabled()) {
             this.isFirstTimeOpen = false
             $(document).ready(async function () {
-                const targetNotePath = options.get('homepageNotePath');
-                const activeNoteContexts = appContext.tabManager.mainNoteContexts;
-                console.log(activeNoteContexts)
-                const matchingNoteContext = activeNoteContexts.find(nc => nc.noteId === targetNotePath.split("/").slice(-1)[0]);
-                if (matchingNoteContext) {
-                    console.log(1)
-                    appContext.tabManager.activateNoteContext(matchingNoteContext.ntxId);
-                }
-                else {
-                    console.log(2)
-                    const result = await appContext.tabManager.openTabWithNoteWithHoisting(targetNotePath);
-                    appContext.tabManager.activateNoteContext(result.ntxId)
-                }
+                //增加延迟，如果去掉会有问题，暂未找到原因
+                setTimeout(async () => {
+                    const targetNotePath = options.get('homepageNotePath');
+                    const activeNoteContexts = appContext.tabManager.mainNoteContexts;
+                    const matchingNoteContext = activeNoteContexts.find(nc => nc.noteId === targetNotePath.split("/").slice(-1)[0]);
+                    if (matchingNoteContext) {
+                        appContext.tabManager.activateNoteContext(matchingNoteContext.ntxId);
+                    }
+                    else {
+                        const result = await appContext.tabManager.openTabWithNoteWithHoisting(targetNotePath);
+                        appContext.tabManager.activateNoteContext(result.ntxId)
+                    }
+                }, 500);
             })
         }
     }
